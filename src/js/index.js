b98025c01ed0d6bd26f29bf103705a25ee2a80bb
Robert Serrano Kobylyansky <robertkobylyansky@gmail.com>
2022-11-04 19:54:25 -0700

    delete old stuff

--- src/js/index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    menu = document.getElementById("mobile-menu");
-    btn = document.getElementById("mobile-menu-button");
-    btn.addEventListener("click", () => {
-        menu.classList.toggle("hidden");
-    });
-});
